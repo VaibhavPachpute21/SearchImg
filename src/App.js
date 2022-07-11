b89259f144c4f9df775afd17da2076be2633d92a
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-07-11 15:06:58 +0530

    axios added response loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Jumbotron from './components/jumbotron';
+import SearchField from './components/SearchField';
+import Images from './components/Images';
+import useAxios from './hooks/useAxios';
+import { createContext } from 'react';
+
+// Create Context
+export const ImageContext=createContext();
+
+
 
 function App() {
+
+  const{response,isLoading,error,fetchData}=useAxios(`search/photos?page=1&query=office&client_id=${process.env.REACT_APP_ACCESS_KEY}`);
+  console.log(response);
+
+  const value={
+    response,
+    isLoading,
+    error,
+    fetchData
+  }
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <ImageContext.Provider value={value}>
+      <Jumbotron>
+        <SearchField/>
+      </Jumbotron>
+      <Images/>
+    </ImageContext.Provider>
   );
 }
 
